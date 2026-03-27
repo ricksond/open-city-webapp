0a7733820faa26a821b52f9a85a14d264229990d
Rickson Dsouza <ricksond2003@gmail.com>
2026-03-27 14:15:13 -0400

    Installed Dependencies

---
+++ my-app/components/ui/animated-container.tsx
@@ -0,0 +1,10 @@
+import { div } from 'framer-motion/client'
+import React from 'react'
+
+export default function AnimatedContainer({
+    children,
+}:{children: React.ReactNode}) {
+    return (
+        <div>Animated container</div>
+    )
+}
