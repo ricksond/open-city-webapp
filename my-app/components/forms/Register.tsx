e1f5cee46ae3c85c8625262d93e0b35f60c70585
Rickson Dsouza <ricksond2003@gmail.com>
2026-03-28 09:19:36 -0400

    Fixed Login and Register Forms with Validation Checks (Frontend)

--- my-app/components/forms/Register.tsx
+++ my-app/components/forms/Register.tsx
@@ -1,52 +1,41 @@
 "use client";
-import { useState } from 'react';
+import { use, useState } from 'react';
 import {motion} from 'framer-motion';
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { Eye,EyeOff } from 'lucide-react';
-import z from "zod";
+import {z} from "zod";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+
 
 const registerSchema=z.object({
     organization:z.string().min(5,"Organization name must be at least 5 characters long"),
     email:z.string().email("Invalid email address"),
-    password:z.string().min(8,"Password must be at least 8 characters long").regex(/[A-Z]/,"Password must contain at least one uppercase letter").regex(/[a-z]/,"Password must contain at least one lowercase letter").regex(/[0-9]/,"Password must contain at least one number").regex(/[@$!%*?&]/,"Password must contain at least one special character"),
+    password:z.string().min(8,"Password must be at least 8 characters long").regex(/[A-Z]/,"Password must contain at least one uppercase letter")
+    .regex(/[a-z]/,"Password must contain at least one lowercase letter").regex(/[0-9]/,"Password must contain at least one number").regex(/[@$!%*?&]/,"Password must contain at least one special character"),
     confirmPassword:z.string().min(8,"Confirm password must be at least 8 characters long") 
 }).refine (data => data.password ===data.confirmPassword,{
-    message:"Passwords don't match",
+    message:"Passwords do not match",
     path:["confirmPassword"]
-})
+});
+
+type registerFormData=z.infer<typeof registerSchema>;
 
 
 export default function Register(){
-    const [organization,setOrganization]=useState("");
-    const [email,setEmail]=useState("");
-    const [password,setPassword]=useState("");
-    const [confirmPassword,setConfirmPassword]=useState("");
     const [showPassword,setShowPassword]=useState(false);
-    const [errors,setErrors]=useState<Record<string,string>>({});
+    const [error,setError]=useState("");
     const [loading,setLoading]=useState(false);
 
-    const handleSubmit=(e:React.FormEvent)=>{
-        e.preventDefault();
-        setErrors({});
-        setLoading(true);
+    const {register,handleSubmit,formState:{errors}}=useForm<registerFormData>({
+    resolver:zodResolver(registerSchema)});
 
-        const result=registerSchema.safeParse({organization,email,password,confirmPassword});
-
-
-        try{
-            // API call to register user
-            console.log("Registering user with data:",result.data);
-        } catch(error){
-            console.log("Registration failed:",error);
-            return setErrors({error:"Registration failed. Please try again."});
-        }finally{
-            setLoading(false);
-        }
+    const submitData=(data:registerFormData)=>{
+     console.log("form Data",data);
     }
-
 
     return (
         <div className="min-h-screen bg-linear-to-br from-primary-50 to-primary-100 flex items-center justify-center p-4">
@@ -62,20 +51,23 @@ export default function Register(){
                             Create an account to get started
                         </p>
                     </div>
-                    <form className='space-y-4' onSubmit={handleSubmit}>
+                    <form className='space-y-4' onSubmit={handleSubmit(submitData)}>
                         <div className="space-y-2">
                             <Label htmlFor='text' className='tracking-wide text-black'>Organization</Label>
-                            <Input id='text' type='text' placeholder='Enter your organization name' value={organization} onChange={(e) => setOrganization(e.target.value)} required />
+                            <Input id='text' type='text' placeholder='Enter your organization name' {...register("organization")}  required />
+                            {errors.organization && <span className="text-red-500">{errors.organization.message}</span>}
                         </div>
                         <div className='space-y-2'>
                             <Label htmlFor='email' className='tracking-wide text-black'>Email</Label>
-                            <Input id="email" type='email' placeholder="Test@richmond.gov" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                            <Input id="email" type='email' placeholder="Test@richmond.gov" {...register("email")}  required />
+                            {errors.email && <span className="text-red-500">{errors.email.message}</span>}
                         </div>
                         <div className="space-y-2">
                             <Label htmlFor='password' className='tracking-wide text-black'>Password</Label>
                             <div className="relative">
-                                <Input id='password' value={password} placeholder='Enter password' onClick={()=>setShowPassword(!showPassword)} required onChange={(e)=> setPassword(e.target.value)}></Input>
-                                <button className='absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500'>
+                                <Input id='password' type="password"  placeholder='Enter password' required {...register("password")} ></Input>
+                                {errors.password && <span className="text-red-500">{errors.password.message}</span>}
+                                <button onClick={()=>setShowPassword(!showPassword)} className='absolute right-3 top-4 transform -translate-y-1/2 text-gray-500'>
                                     {showPassword ? <EyeOff /> : <Eye />}
                                 </button>
                             </div>
@@ -83,8 +75,9 @@ export default function Register(){
                         <div className='space-y-2'>
                             <Label htmlFor='confirmPassword' className='tracking-wide text-black'>Confirm Password</Label>
                             <div className="relative">
-                                <Input id='confirmPassword' value={confirmPassword} placeholder='Confirm password' onClick={()=>setShowPassword(!showPassword)} required onChange={(e)=> setConfirmPassword(e.target.value)}></Input>
-                                <button className='absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500'>
+                                <Input id='confirmPassword'type={showPassword? "text" : "password"}  {...register("confirmPassword")} placeholder='Confirm password' required ></Input>
+                                {errors.confirmPassword && <span className="text-red-500">{errors.confirmPassword.message}</span>}
+                                <button onClick={()=>setShowPassword(!showPassword)}  className='absolute right-3 top-4 transform -translate-y-1/2 text-gray-500'>
                                     {showPassword ? <EyeOff /> : <Eye />}
                                 </button>
                             </div>
