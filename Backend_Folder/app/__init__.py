49ebea6f2f9c78f5de951bcaff5dde47b9789a80
Taksheelshetty <120228028+Taksheelshetty@users.noreply.github.com>
2026-03-28 02:25:15 -0400

    Updated Backend

--- Backend_Folder/app/__init__.py
+++ Backend_Folder/app/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
+from .firebase import db, pb_auth, auth
+from .routes import register_blueprints
 import os
 
 
 def create_app():
     app = Flask(__name__)
-
     CORS(
         app,
         supports_credentials=True,
@@ -13,15 +14,9 @@ def create_app():
         allow_headers=["Content-Type", "Authorization"],
         expose_headers=["Content-Type", "Authorization"]
     )
-
     app.secret_key = os.getenv("SESSION_SECRET")
-
-    from .firebase import db, pb_auth, auth
     app.db = db
     app.pb_auth = pb_auth
     app.auth = auth
-
-    from .routes import register_blueprints
     register_blueprints(app)
-
     return app
--- Backend_Folder/app/routes/auth_routes.py
+++ Backend_Folder/app/routes/auth_routes.py
@@ -1,12 +1,15 @@
 from flask import Blueprint, request, jsonify, current_app as app
 import json
+from firebase_admin import firestore
 
 auth_bp = Blueprint("auth", __name__)
 
 @auth_bp.errorhandler(Exception)
 def handle_exceptions(e):
     error_str = str(e)
+    print(error_str)
     firebase_message = "INVALID_EMAIL_OR_PASSWORD"
+
     if "EMAIL_EXISTS" in error_str:
         firebase_message = "EMAIL_EXISTS"
     elif "INVALID_PASSWORD" in error_str:
@@ -23,6 +26,7 @@ def handle_exceptions(e):
             firebase_message = error_json.get("error", {}).get("message", "UNKNOWN_ERROR")
         except Exception:
             firebase_message = "UNKNOWN_ERROR"
+
     return jsonify({"error": {"message": firebase_message}}), 400
 
 
@@ -31,28 +35,27 @@ def register():
     data = request.get_json() or {}
     email = data.get("email")
     password = data.get("password")
-    name = data.get("name", "")
-    weight = data.get("weight", "")
-    preferredFood = data.get("preferredFood", "")
-    cuisine = data.get("cuisine", "")
-    diet = data.get("diet", "")
-    allegories = data.get("allegories", "")
-    if not email or not password:
-        return jsonify({"error": {"message": "EMAIL_AND_PASSWORD_REQUIRED"}}), 400
+    organization_name = data.get("organization_name")
+    print(email, password, organization_name)
+    if not email or not password :
+        return jsonify({
+            "error": {
+                "message": "EMAIL_PASSWORD_ORG_REQUIRED"
+            }
+        }), 400
     user = app.pb_auth.create_user_with_email_and_password(email, password)
     uid = user['localId']
     id_token = user['idToken']
     app.db.collection("users").document(uid).set({
         "email": email,
-        "name": name,
-        "weight": weight,
-        "preferredFood": preferredFood,
-        "cuisine": cuisine,
-        "diet": diet,
-        "allegories": allegories,
-        "user_preferences": {"liked_tags": {}, "disliked_tags": {}},
+        "organization_name": organization_name,
+        "created_at": firestore.SERVER_TIMESTAMP
     })
-    return jsonify({"message": "User registered", "uid": uid, "idToken": id_token}), 201
+    return jsonify({
+        "message": "User registered",
+        "uid": uid,
+        "idToken": id_token
+    }), 201
 
 
 @auth_bp.route('/login', methods=['POST'])
@@ -60,7 +63,6 @@ def login():
     data = request.get_json() or {}
     email = data.get("email")
     password = data.get("password")
-
     if not email or not password:
         return jsonify({"error": {"message": "EMAIL_AND_PASSWORD_REQUIRED"}}), 400
     user = app.pb_auth.sign_in_with_email_and_password(email, password)
@@ -69,7 +71,6 @@ def login():
         "idToken": user['idToken'],
         "uid": user['localId']
     }), 200
-
 
 @auth_bp.route('/logout', methods=['POST'])
 def logout():
